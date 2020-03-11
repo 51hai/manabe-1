0208ce3b550a1d0c9c43c0df894ff097e869d5b1
yanxinfire <yan740487738@qq.com>
2020-03-11 13:08:21 +0800

    add images of project display

--- deploy/urls.py
+++ deploy/urls.py
@@ -11,7 +11,7 @@ app_name = 'deploy'
 urlpatterns = [
     path('create/', login_required(DeployCreateView.as_view()), name='create'),
     path('list/', login_required(DeployListView.as_view()), name='list'),
-    path('view/<int:pk>/', login_required(DeployDetailView.as_view()), name='detail'),
+    path('view/<slug:pk>/', login_required(DeployDetailView.as_view()), name='detail'),
     path('jenkins_build/', login_required(jenkins_build), name='jenkins_build'),
     path('jenkins_status/', login_required(jenkins_status), name='jenkins_status'),
     path('update_deploypool_jenkins/', login_required(update_deploypool_jenkins),
